98dc77f513e5374fab004ed26b827cfeb29baab7
laiwei <laiwei.ustc@gmail.com>
2017-03-07 10:56:16 +0800

    add logger

--- rrd/__init__.py
+++ rrd/__init__.py
@@ -7,12 +7,17 @@ from flask import Flask
 app = Flask(__name__)
 app.config.from_object("rrd.config")
 
+from rrd.utils.logger import logging
+log = logging.getLogger(__file__)
+
 @app.errorhandler(Exception)
 def all_exception_handler(error):
-    print traceback.format_exc()
-    return u'dashboard 暂时无法访问，请联系管理员', 500
+    tb = traceback.format_exc()
+    err_msg = u'dashboard 暂时无法访问,请联系管理员.\nerror:%s\ntraceback日志如下:%s' %(error, tb)
+    return err_msg, 500
 
 from view import index
+from view import auth
+from view import user, team
 from view import api
 from view import chart, screen
-from view import auth, user, team
